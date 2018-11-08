058dcf036e0222df2a52983f431c624d3ff23a03
星野恵瑠 <shonetesmith@gmail.com>
2018-11-08 21:48:13 +0900

    Xcode init project

---
+++ Chainit/Chainit.h
@@ -0,0 +1,19 @@
+//
+//  Chainit.h
+//  Chainit
+//
+//  Created by 史翔新 on 2018/11/08.
+//  Copyright © 2018 Crazism. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chainit.
+FOUNDATION_EXPORT double ChainitVersionNumber;
+
+//! Project version string for Chainit.
+FOUNDATION_EXPORT const unsigned char ChainitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chainit/PublicHeader.h>
+
+
